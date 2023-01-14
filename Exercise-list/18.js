5579a943f0aa937c915e7a22cdb40383c6ab6c35
Pedro Victor <pedro20002victor@gmail.com>
2023-01-14 10:42:13 -0300

    Ex 38

---
+++ Exercise-list/18.js
@@ -0,0 +1,7 @@
+function invertOrder(arr) {
+  arr.reverse();
+  return arr;
+}
+
+const numbers = [1, 2, 3, 4, 5, 6];
+console.log(invertOrder(numbers));
---
+++ hard-list/38.js
@@ -0,0 +1,10 @@
+function psOrNg(num) {
+  if (num >= 0) {
+    return console.log(`${num} e positivo`);
+  }
+  return console.log(`${num} e negativo`);
+};
+
+psOrNg(10);
+psOrNg(-89);
+psOrNg(0);
