6d05a8727ad56bbc41245cd595e47d9806159340
Pedro Victor <pedro20002victor@gmail.com>
2023-01-31 08:48:15 -0300

    Helpers

---
+++ javascript-modern/Helpers.js
@@ -0,0 +1,9 @@
+const tech = 'Noje.js';
+const array = ['Node.js', 'React', 'Typescript'];
+
+const includes = tech.includes('.js'); // Nao precisa ser valor exato
+const includeArr = array.includes('React'); // Valor exato
+
+const startsWith = tech.startsWith('N'); // Comeca com - endsWith segue o mesmo principio
+
+console.log(startsWith);
